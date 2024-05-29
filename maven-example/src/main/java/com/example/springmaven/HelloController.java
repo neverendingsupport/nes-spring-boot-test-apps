4ea01685c9231b88725511e66299b25c519687a2
hdtmccallie <trey@herodevs.com>
2024-05-29 13:47:25 -0700

    Initial maven app

---
+++ maven-example/src/main/java/com/example/springmaven/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.springmaven;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  // private final NesTestService nesTestService = new NesTestService();
+
+  @GetMapping("/")
+  public String index() {
+    return "Success! This is a Spring Boot application with Maven.";
+  }
+
+}
