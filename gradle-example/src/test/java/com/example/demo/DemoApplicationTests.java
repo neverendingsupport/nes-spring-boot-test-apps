67203fc8f902e322a59c5dc9d26ae8c905477867
Ryan Murphy <ryanm@herodevs.com>
2025-01-17 12:24:03 -0700

    Sample update using Spring NES Foundations 4.x

--- gradle-example/src/test/java/com/example/demo/DemoApplicationTests.java
+++ gradle-example/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+  public void contextLoads() {
 	}
 
 }
